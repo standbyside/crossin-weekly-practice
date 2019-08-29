07544e8d921ec4826a0c0e411af14209d9518dd5
Jerry <smart1jerry@outlook.com>
2019-08-29 23:04:21 +0800

    [feat] add search-files solution

--- 查找文件.py
+++ 查找文件.py
@@ -4,3 +4,35 @@
 """
 找出指定文件夹中的所有以txt结尾的文件，包括所有嵌套的子文件夹
 """
+
+import fnmatch
+import os
+
+
+def search_file_1(file_path, txt_list=None):
+    """解法1."""
+    if txt_list is None:
+        txt_list = []
+    for f in os.listdir(file_path):
+        abs_path = os.path.join(file_path, f)
+        if os.path.isdir(abs_path):
+            search_file_1(abs_path, txt_list)
+        elif os.path.splitext(abs_path)[1] == '.txt':
+            txt_list.append(f)
+    return txt_list
+
+
+def search_file_2(input_dir):
+    """解法2."""
+    txt_list = []
+    for parent, dir_names, file_names in os.walk(input_dir):
+        for file_name in file_names:
+            txt_list.append(file_name)
+    return fnmatch.filter(txt_list, '*.txt')
+
+
+L = search_file_1(r"D:\workspace")
+print(L)
+
+L = search_file_2(r"D:\workspace")
+print(L)
--- 查找文件内容.py
+++ 查找文件内容.py
@@ -5,3 +5,23 @@
 找出指定文件夹中的所有以txt结尾的文件，包括所有嵌套的子文件夹
 在此基础上列出这个文件夹（含所有子文件夹）里，所有文件内容包括指定搜索文本的文件
 """
+
+import os
+
+
+def search_file_content(input_dir, key_word):
+    """解法1."""
+    txt_list = []
+    for parent, dir_names, file_names in os.walk(input_dir):
+        for file_name in file_names:
+            abs_path = os.path.join(parent, file_name)
+            if os.path.splitext(abs_path)[1] != '.txt':
+                continue
+            with open(abs_path, 'r', encoding='utf-8') as f:
+                if key_word in f.read():
+                    txt_list.append(file_name)
+    return txt_list
+
+
+L = search_file_content(r"D:\workspace\my-python", '查找')
+print(L)
