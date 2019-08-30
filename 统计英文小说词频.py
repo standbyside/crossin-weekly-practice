aeb836fc1bd05ee49ca4430ede7e7759c2efaf86
Jerry <smart1jerry@outlook.com>
2019-08-30 17:31:23 +0800

    [feat] add count-words solution

--- 统计英文小说词频.py
+++ 统计英文小说词频.py
@@ -5,3 +5,14 @@
 【基础题】：统计一部英文小说里单词的出现次数（忽略大小写），按出现次数显示最高的 100 个单词
 【附加题】：多统计几个不同作家的作品，挑选一些特征词汇的次数画在图表上，展示不同作家的风格区别。
 """
+
+from collections import Counter
+
+
+def count_words(file_path):
+    text = open(file_path).read()
+    common_words = Counter(text.split(' ')).most_common(100)
+    return common_words
+
+
+print(count_words(r'/Users/zn/Downloads/word-cloud-text.txt'))
