2bfbe8007f9360a0d78470ab455010ef094bd0d4
Jerry <smart1jerry@outlook.com>
2019-08-30 14:50:17 +0800

    [feat] add programmer's romance solution

--- 程序猿的浪漫.py
+++ 程序猿的浪漫.py
@@ -15,3 +15,20 @@
 
 请各位大侦探们使用科学的方法算出我说的什么吧！
 """
+
+import hashlib
+import base64
+
+
+def decrypt():
+    password = 'NDRiMWZmMmVjZTk5MTFjMWI1MDNkYTY0MzZlYTAzMTA='
+    answers = ['我们在一起吧', '我选择原谅你', '别说话，吻我', '多喝热水']
+    for answer in answers:
+        md5_str = hashlib.md5(answer.encode('utf-8')).hexdigest()
+        base64_byte = base64.b64encode(md5_str.encode('utf-8'))
+        if str(base64_byte, 'utf-8') == password:
+            return answer
+    raise ValueError("can't find answer")
+
+
+assert decrypt() == '多喝热水'
