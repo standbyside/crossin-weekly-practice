641b940117d2a3d70f5a10b22e5ec92c128fb6bb
Jerry <smart1jerry@outlook.com>
2019-08-30 14:58:58 +0800

    [docs] format problems‘ descriptions

--- 程序猿的浪漫.py
+++ 程序猿的浪漫.py
@@ -2,18 +2,20 @@
 # -*- coding: utf-8 -*-
 
 """
-在 python 中有 hashlib 和 base64 两大加密模块，
-将一串字符串先经过 hashlib.md5 加密，然后再经过 base64 加密，最后得到一串字符：
-'NDRiMWZmMmVjZTk5MTFjMWI1MDNkYTY0MzZlYTAzMTA=\n'
+【基础题】：
 
-在此给出 4 个选项
+    在 python 中有 hashlib 和 base64 两大加密模块，
+    将一串字符串先经过 hashlib.md5 加密，然后再经过 base64 加密，最后得到一串字符：
+    'NDRiMWZmMmVjZTk5MTFjMWI1MDNkYTY0MzZlYTAzMTA=\n'
 
-    我们在一起吧
-    我选择原谅你
-    别说话，吻我
-    多喝热水
+    在此给出 4 个选项
 
-请各位大侦探们使用科学的方法算出我说的什么吧！
+        我们在一起吧
+        我选择原谅你
+        别说话，吻我
+        多喝热水
+
+    请各位大侦探们使用科学的方法算出我说的什么吧！
 """
 
 import hashlib
