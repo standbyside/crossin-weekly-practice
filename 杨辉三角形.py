4b3e70ab0d5645c05253e6d7814fa8b42de0a358
Jerry <smart1jerry@outlook.com>
2019-09-01 13:45:59 +0800

    [feat] add yang-hui-triangle solution

--- 杨辉三角形.py
+++ 杨辉三角形.py
@@ -2,33 +2,40 @@
 # -*- coding: utf-8 -*-
 
 """
-【基础题】：
-    定义一个函数 yanghui() ，传入正整数参数 M、N，分别代表杨辉三角形第 M 行，左起第 N 个数字（M,N 都从 0 开始计算）。
-    入超出范围则返回 invalid query 。
-
-【附加题】：
-    生成杨辉三角形。
-    定义一个函数 generate_yh() 传入整数参数 M < 1000，生成前 M 行杨辉三角形。
+【基础题】：传入正整数参数 M、N，返回杨辉三角形第 M 行，左起第 N 个数字（M，N都从0开始计算）
+【附加题】：传入整数参数 M < 1000，生成前 M 行杨辉三角形
 """
 
 
-def yanghui(m, n):
-    """
-    >>> yanghui(1,1)
-    1
-    >>> yanghui(3,2)
-    3
-    >>> yanghui(1,4)
-    invalid query
-    """
-    pass
+def base_yh(m, n):
+    if m < 0 or n < 0 or not isinstance(m, int) or not isinstance(n, int) or n > m:
+        raise ValueError('非法入参')
+    last_line = []
+    for i in range(0, m + 1):
+        cur_line = [1]
+        if i > 0:
+            for j in range(0, len(last_line) - 1):
+                cur_line.append(last_line[j] + last_line[j + 1])
+            cur_line.append(1)
+        last_line = cur_line
+    return last_line[n]
 
 
 def generate_yh(m):
-    """
-    >>> generate_yh(3)
-    1
-    1 1
-    1 2 1
-    """
-    pass
+    if m < 0 or m > 1000 or not isinstance(m, int):
+        raise ValueError('请输入大于0小于1000的正整数')
+    last_line = []
+    for i in range(0, m + 1):
+        cur_line = [1]
+        if i > 0:
+            for j in range(0, len(last_line) - 1):
+                cur_line.append(last_line[j] + last_line[j + 1])
+            cur_line.append(1)
+        print(cur_line)
+        last_line = cur_line
+
+
+assert base_yh(1, 1) == 1
+assert base_yh(3, 2) == 3
+
+generate_yh(10)
